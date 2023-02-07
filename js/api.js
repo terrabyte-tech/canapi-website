bbff0be91d2cb45c1b05b37714e2df4c851d6316
sharper <hi@sammyharper.com>
2023-02-07 17:07:28 -0600

    added a beginning JSON to read from, began structuring how the post request will be formatted

--- js/api.js
+++ js/api.js
@@ -2,6 +2,14 @@ var currentURL = window.location.host;
 
 console.log("Canapi api.js loaded in " + currentURL);
 
+var date = new Date().toDateString;
+
+var actionBody = {
+  domain: currentURL,
+  user: 'test',
+  date: currentDate
+};
+
 function canapiAction(){
   if(currentURL == "www.dev.terrabyte.eco" || currentURL == "www.dev.canapi.io"){
     console.log("terrabyte action logged");
