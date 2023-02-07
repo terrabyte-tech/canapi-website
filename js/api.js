37d94ec634caca94bc27489b108477cffee8f8a8
sharper <hi@sammyharper.com>
2023-02-07 15:04:56 -0600

    swapped document referrer check to window.location check

--- js/api.js
+++ js/api.js
@@ -1,7 +1,9 @@
-console.log("api.js loaded in " + document.referrer);
+// console.log("api.js loaded in " + document.referrer);
+const currentURL = window.location.href;
+console.log("api.js loaded in " + currentURL);
 
 function canapiAction(){
-  if(document.referrer == "https://terrabyte.eco" || document.referrer == "https://dev.terrabyte.eco"){
+  if(currentURL == "https://terrabyte.eco" || currentURL == "https://dev.terrabyte.eco"){
     console.log("terrabyte action logged");
   }
   console.log("canapi action logged!");
