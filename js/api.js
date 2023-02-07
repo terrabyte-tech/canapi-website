04e02d3919e2811d788864dd7d9a647d3cbc60cf
sharper <hi@sammyharper.com>
2023-02-07 15:11:39 -0600

    sanitizing the current URL

--- js/api.js
+++ js/api.js
@@ -1,9 +1,11 @@
 // console.log("api.js loaded in " + document.referrer);
-const currentURL = window.location.href;
+var currentURL = window.location.href;
+currentURL = currentURL.split("//").pop();
+
 console.log("api.js loaded in " + currentURL);
 
 function canapiAction(){
-  if(currentURL == "https://terrabyte.eco" || currentURL == "https://dev.terrabyte.eco"){
+  if(currentURL == "dev.terrabyte.eco" || currentURL == "dev.canapi.io"){
     console.log("terrabyte action logged");
   }
   console.log("canapi action logged!");
