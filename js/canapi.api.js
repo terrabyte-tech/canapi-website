05be4782101721f82fe6df2e955a2d3c34cf06ea
sharper <hi@sammyharper.com>
2023-02-08 12:39:42 -0600

    added window listener so code only runs after the window has loaded

--- js/canapi.api.js
+++ js/canapi.api.js
@@ -1,42 +1,46 @@
-var currentURL = window.location.host;
-var key = null;
+window.addEventListener("load", function(){
 
-console.log("canapi.api.js loaded in " + currentURL);
-canapiAction(key);
+  var currentURL = window.location.host;
+  var key = null;
 
-
-// create POST message
-var currentDate = new Date().toDateString;
-var actionBody = {
-  domain: currentURL,
-  user: 'test',
-  date: currentDate
-};
+  console.log("canapi.api.js loaded in " + currentURL);
+  canapiAction(key);
 
 
-// listen for clicks on canapi click triggers
-var canapiTriggers = document.querySelectorAll("[data-canapi-click-trigger]");
 
-if(canapiTriggers.length > 0){
-  for(let x = 0; x < canapiTriggers.length; x++){
-    canapiTriggers[x].addEventListener("click", function(){
+  // listen for clicks on canapi click triggers
+  var canapiTriggers = document.querySelectorAll("[data-canapi-click-trigger]");
 
-      console.log("clicked a canapi trigger!");
-      key = this.getAttribute("data-key");
-      canapiAction(key);
-    })
+  if(canapiTriggers.length > 0){
+    for(let x = 0; x < canapiTriggers.length; x++){
+      canapiTriggers[x].addEventListener("click", function(){
+
+        console.log("clicked a canapi trigger!");
+        key = this.getAttribute("data-key");
+        canapiAction(key);
+      })
+    }
   }
-}
 
 
-function canapiAction(key){
-  if(currentURL == "www.dev.terrabyte.eco" || currentURL == "www.dev.canapi.io"){
-    console.log("terrabyte action logged");
-    console.log("key: " + key);
+  function canapiAction(key){
+    if(currentURL == "www.dev.terrabyte.eco" || currentURL == "www.dev.canapi.io"){
+      console.log("terrabyte action logged");
+      console.log("key: " + key);
+    }
+    else{
+      console.log("request from unknown domain, unable to verify contribution");
+      console.log("attempted key: " + key);
+    }
   }
-  else{
-    console.log("request from unknown domain, unable to verify contribution");
-    console.log("attempted key: " + key);
-  }
+
   
-}
+  // create POST message
+  var currentDate = new Date().toDateString;
+  var actionBody = {
+    domain: currentURL,
+    user: 'test',
+    date: currentDate
+  };
+  
+}, false);
